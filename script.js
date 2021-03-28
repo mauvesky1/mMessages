ae057a8e4e7d664d770eed60419f7f395d849045
bob <rob.draper@live.com>
2021-03-28 13:32:08 +0100

    First

---
+++ script.js
@@ -0,0 +1,9 @@
+ const type1 = ["a", "b", "c"]
+ const type2 = ["d", "e", "f"]
+ const type3 = ["1", "2", "3"]
+
+randomNumber1 = Math.floor(Math.random() * 3)
+randomNumber2 = Math.floor(Math.random() * 3)
+randomNumber3 = Math.floor(Math.random() * 3)
+
+console.log("Hello")
